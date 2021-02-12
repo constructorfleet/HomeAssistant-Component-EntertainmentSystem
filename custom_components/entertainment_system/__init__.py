a69d2c781b960757b07db917bab4a7cd27882eff
Teagan glenn <teagan.m.glenn@gmail.com>
2021-02-12 16:02:30 -0700

    Initial component - WIP

---
+++ custom_components/entertainment_system/__init__.py
@@ -0,0 +1,10 @@
+"""
+Entertainment System Component
+https://github.com/ConstructorFleet/HomeAssistant-Component-EntertainmentSystem
+"""
+import logging
+from itertools import chain
+
+from homeassistant.const import MATCH_ALL
+
+_LOGGER = logging.getLogger(__name__)
---
+++ custom_components/entertainment_system/config_flow.py
@@ -0,0 +1,1 @@
+"""Configuration flow for Entertainment System integration."""

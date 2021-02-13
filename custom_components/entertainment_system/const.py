d316e6a507da2fb3c8cfa6ab43c907dac4a2f7f6
Teagan glenn <teagan.m.glenn@gmail.com>
2021-02-13 10:39:30 -0700

    Constants

--- custom_components/entertainment_system/const.py
+++ custom_components/entertainment_system/const.py
@@ -1,46 +1,68 @@
 """ Constants related to Entertainment System."""
+from homeassistant.const import (
+  CONF_NAME,
+  CONF_DEFAULT,
+  STATE_IDLE,
+  STATE_OFF,
+  STATE_ON,
+  STATE_PAUSED,
+  STATE_PLAYING,
+  STATE_UNAVAILABLE,
+  STATE_UNKNOWN,
+  )
+from homeassistant.components.remote import DOMAIN as REMOTE_DOMAIN
+from homeassistant.components.media_player.const import (
+  DOMAIN as MEDIA_PLAYER_DOMAIN,
+  ATTR_APP_ID,
+  ATTR_APP_NAME,
+  ATTR_INPUT_SOURCE,
+  ATTR_INPUT_SOURCE_LIST,
+  ATTR_MEDIA_ALBUM_ARTIST,
+  ATTR_MEDIA_ALBUM_NAME,
+  ATTR_MEDIA_ARTIST,
+  ATTR_MEDIA_CHANNEL,
+  ATTR_MEDIA_CONTENT_ID,
+  ATTR_MEDIA_CONTENT_TYPE,
+  ATTR_MEDIA_DURATION,
+  ATTR_MEDIA_ENQUEUE,
+  ATTR_MEDIA_EXTRA,
+  ATTR_MEDIA_EPISODE,
+  ATTR_MEDIA_PLAYLIST,
+  ATTR_MEDIA_POSITION,
+  ATTR_MEDIA_POSITION_UPDATED_AT,
+  ATTR_MEDIA_REPEAT,
+  ATTR_MEDIA_SEASON,
+  ATTR_MEDIA_SEEK_POSITION,
+  ATTR_MEDIA_SERIES_TITLE,
+  ATTR_MEDIA_SHUFFLE,
+  ATTR_MEDIA_TITLE,
+  ATTR_MEDIA_TRACK,
+  ATTR_MEDIA_VOLUME_LEVEL,
+  ATTR_MEDIA_VOLUME_MUTED,
+  ATTR_SOUND_MODE,
+  ATTR_SOUND_MODE_LIST,
+  )
 
 DOMAIN = "entertainment_system"
 
 CONF_AVAILABILITY = "availability"
-CONF_POWER_STATE = "power_state"
-CONF_MEDIA_STATE = "media_state"
+CONF_DISPLAY = "display"
+CONF_POWER = "power"
+CONF_TURN_ON_ACTION = "turn_on_action"
+CONF_TURN_OFF_ACTION = "turn_off_action"
+CONF_POWER_STATE_TEMPLATE = "power_state_template"
+CONF_MEDIA_SOURCES = "media_sources"
+CONF_MEDIA_PLAYER = "media_player"
 CONF_REMOTE = "remote"
-CONF_SPEAKER = "speaker"
 
-#
-#
-# CONF_AVAILABILITY_TEMPLATE = "availability_template"
-# CONF_MEDIAPLAYER = "media_players"
-# ON_ACTION = "turn_on"
-# OFF_ACTION = "turn_off"
-# PLAY_ACTION = "play"
-# STOP_ACTION = "stop"
-# PAUSE_ACTION = "pause"
-# NEXT_ACTION = "next"
-# PREVIOUS_ACTION = "previous"
-# VOLUME_UP_ACTION = "volume_up"
-# VOLUME_DOWN_ACTION = "volume_down"
-# MUTE_ACTION = "mute"
-# CURRENT_SOURCE_TEMPLATE = "current_source_template"
-# CONF_INPUTS = "inputs"
-# TITLE_TEMPLATE = "title_template"
-# ARTIST_TEMPLATE = "artist_template"
-# ALBUM_TEMPLATE = "album_template"
-# CURRENT_VOLUME_TEMPLATE = "current_volume_template"
-# CURRENT_IS_MUTED_TEMPLATE = "current_is_muted_template"
-# ALBUM_ART_TEMPLATE = "album_art_template"
-# SET_VOLUME_ACTION = "set_volume"
-# PLAY_MEDIA_ACTION = "play_media"
-# MEDIA_CONTENT_TYPE_TEMPLATE = "media_content_type_template"
-# MEDIA_IMAGE_URL_TEMPLATE = "media_image_url_template"
-# MEDIA_EPISODE_TEMPLATE = "media_episode_template"
-# MEDIA_SEASON_TEMPLATE = "media_season_template"
-# MEDIA_SERIES_TITLE_TEMPLATE = "media_series_title_template"
-# MEDIA_ALBUM_ARTIST_TEMPLATE = "media_album_artist_template"
-# SEEK_ACTION = "seek"
-# CURRENT_POSITION_TEMPLATE = "current_position_template"
-# MEDIA_DURATION_TEMPLATE = "media_duration_template"
-# CURRENT_SOUND_MODE_TEMPLATE = "current_sound_mode_template"
-# CONF_SOUND_MODES = "sound_modes"
-# CONF_UNIQUE_ID = "unique_id"
+VALID_STATES = [
+    STATE_ON,
+    STATE_OFF,
+    "true",
+    "false",
+    STATE_IDLE,
+    STATE_PAUSED,
+    STATE_PLAYING,
+    STATE_UNKNOWN,
+    STATE_UNAVAILABLE
+    ]

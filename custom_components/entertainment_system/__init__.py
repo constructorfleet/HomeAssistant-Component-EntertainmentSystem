2a0118fae4ba8a7bdff7fbf3894d1a126cc8c6aa
Teagan glenn <teagan.m.glenn@gmail.com>
2021-02-14 07:50:25 -0700

    Remove unnecesssary code

--- custom_components/entertainment_system/__init__.py
+++ custom_components/entertainment_system/__init__.py
@@ -2,9 +2,4 @@
 Entertainment System Component
 https://github.com/ConstructorFleet/HomeAssistant-Component-EntertainmentSystem
 """
-import logging
-from itertools import chain
 
-from homeassistant.const import MATCH_ALL
-
-_LOGGER = logging.getLogger(__name__)
